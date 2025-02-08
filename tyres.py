ec523fe9291e926446c5d487e65fa9be5a25f690
yngmzleen <160007357+yngmzleen@users.noreply.github.com>
2025-02-08 17:53:18 +0700

    Update tyres.py

--- tyres.py
+++ tyres.py
@@ -22,7 +22,7 @@ def normalize_fields(item):
         "priceOpt": "opt",
         "price": "price",
         "spikes": "thorn",
-        "img_big_my": "img",
+        "img_big_my": "img_small",
         "proizvoditel": "brand"
     }
     
